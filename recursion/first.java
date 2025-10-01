c082bc30b31c86d64d8da1f061b448f68317e50a
Shadow-31 <146632708+Shadow-31@users.noreply.github.com>
2025-10-01 12:16:19 +0530

    Add files via upload

---
+++ recursion/first.java
@@ -0,0 +1,19 @@
+package DSAP.recursion;
+
+class first {
+    // recursion program
+    public static void main(String[] args) {
+        int n = 5;
+        int result = factorial(n);
+        System.out.println("Factorial of " + n + " is " + result);
+        }
+        public static int factorial(int n) {
+            if (n == 0 || n == 1) {
+                return 1;
+                }
+                else {
+                    return (n * factorial(n - 1));
+                    }
+                    
+    }
+}
